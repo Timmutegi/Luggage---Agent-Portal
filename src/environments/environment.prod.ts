d574a796d9a2abea5bcf2ba938592ec1d2f9cedd
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-24 19:09:13 +0300

    added space between back and submit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBavo9Wf2YeY_76-qCIEMlPNHtaHKwA5Uk',
+    authDomain: 'morbags-agent.firebaseapp.com',
+    databaseURL: 'https://morbags-agent.firebaseio.com',
+    projectId: 'morbags-agent',
+    storageBucket: 'morbags-agent.appspot.com',
+    messagingSenderId: '874816729628',
+    appId: '1:874816729628:web:2906213fb44159036e25d9',
+    measurementId: 'G-9BYE5W9YXK'
+  }
 };
