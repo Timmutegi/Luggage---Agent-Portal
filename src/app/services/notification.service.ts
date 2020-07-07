ef1d811eaed079e2abe3361f605d2e4ea99ebad8
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-07 10:45:54 +0300

    Added success notification

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -7,11 +7,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class NotificationService {
 
   constructor(public snackBar: MatSnackBar) { }
-
-    showError(message: string): void {
-    console.log('message');
-    // The second parameter is the text in the button.
-    // In the third, we send in the css class for the snack bar.
+  showSuccess(message: string): void {
+    this.snackBar.open(message, 'X', {
+      duration: 3000,
+      verticalPosition: 'top'
+    });
+  }
+  showError(message: string): void {
     this.snackBar.open(message, 'X', {
       duration: 5000,
       verticalPosition: 'top'
