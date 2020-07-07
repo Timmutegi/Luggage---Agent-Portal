bdc2d27f94aa158b2225d5801c6d33aee33a18de
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-07-07 10:12:56 +0300

    Added subscribe button and social media links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  subscribeForm: FormGroup;
+  submitted = false;
+  clicked = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit() {
+    this.subscribeForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]]
+    });
+  }
+
+  get f() {
+    return this.subscribeForm.controls;
+  }
+
+  subscribe() {
+    this.submitted = true;
+
+    if (this.subscribeForm.invalid) {
+      return;
+    }
+
+    this.clicked = true;
+
+    this.api.post('/subscribe', this.subscribeForm.value).subscribe(
+      res => {
+        if (res.code === 200) {
+          this.submitted = false;
+          this.subscribeForm.reset();
+          // this.flashMessage.show('You are now subscribed', {cssClass: 'p-1', timeout: 10000});
+          this.clicked = false;
+        }
+      },
+      err => {
+        this.clicked = false;
+        // this.errorHandler.handleError(err);
+      }
+    );
   }
 
 }
